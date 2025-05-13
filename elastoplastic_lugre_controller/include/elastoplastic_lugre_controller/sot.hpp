6061d183176ac7477a95a2fa6cd516a69299bd94
RobertoFausti <roberto.fausti@stiima.cnr.it>
2025-05-13 10:20:42 +0200

    [wip] add weights matrices to tasks in sot

--- elastoplastic_lugre_controller/include/elastoplastic_lugre_controller/sot.hpp
+++ elastoplastic_lugre_controller/include/elastoplastic_lugre_controller/sot.hpp
@@ -13,25 +13,33 @@ private:
   Eigen::VectorXd m_Ft;
   Eigen::MatrixXd m_Ad;
   Eigen::VectorXd m_bd;
+  Eigen::MatrixXd m_W;
 
 public:
-  Task(const size_t problem_size, const size_t task_size) : m_prb_dim(problem_size), m_task_dim(task_size) {
-    m_Gt = Eigen::MatrixXd::Zero(m_prb_dim, m_prb_dim);
-    m_Ft = Eigen::VectorXd::Zero(m_prb_dim);
-    m_Ad = Eigen::MatrixXd::Zero(m_task_dim, m_prb_dim);
-    m_bd = Eigen::VectorXd::Zero(m_task_dim);
+  Task(const size_t problem_size, const size_t task_size)
+      : m_prb_dim(problem_size), m_task_dim(task_size), m_Gt(problem_size, problem_size), m_Ft(problem_size),
+        m_Ad(task_size, problem_size), m_bd(task_size), m_W(task_size, task_size) {
+    m_Gt.setZero();
+    m_Ft.setZero();
+    m_Ad.setZero();
+    m_bd.setZero();
+    m_W.setIdentity();
   }
 
   Eigen::MatrixXd& A() { return m_Ad; }
   Eigen::VectorXd& b() { return m_bd; }
+  Eigen::MatrixXd& W() { return m_W; }
   Eigen::MatrixXd G() const { return m_Gt; }
   Eigen::VectorXd F() const { return m_Ft; }
   Eigen::VectorXd value(const Eigen::VectorXd& x) { return m_Ad * x + m_bd; }
   void update_task(void) {
-    m_Gt = m_Ad.transpose() * m_Ad;
-    m_Ft = m_bd.transpose() * m_Ad;
+    m_Gt = m_Ad.transpose() * m_W * m_Ad;
+    m_Ft = m_bd.transpose() * m_W * m_Ad;
   }
+  size_t size() const { return m_task_dim; }
+  size_t problem_size() const { return m_prb_dim; }
 };
+
 
 class Stack {
 private:
@@ -42,18 +50,18 @@ private:
   Eigen::MatrixXd m_F;
 
 public:
-  Stack(const size_t problem_size, const double level_step = 1e-3)
-      : m_prb_dim(problem_size), m_level(0), m_level_step(level_step) {
+  Stack(const size_t problem_size, const double level_step = 1e-3, const double level_zero = 0)
+      : m_prb_dim(problem_size), m_level(level_zero), m_level_step(level_step) {
     clear();
   }
   Eigen::MatrixXd G() const { return m_G; }
   Eigen::MatrixXd F() const { return m_F; }
   double level_step() const { return m_level_step; }
   int new_level(void) { return ++m_level; }
-  void push_task(Task& t) {
+  void push_task(Task& t, const double relative_task_weight = 1.0) {
     t.update_task();
-    m_G += t.G() * std::pow(m_level_step, m_level);
-    m_F += t.F() * std::pow(m_level_step, m_level);
+    m_G += t.G() * relative_task_weight * std::pow(m_level_step, m_level);
+    m_F += t.F() * relative_task_weight * std::pow(m_level_step, m_level);
   }
   void clear(void) {
     m_G = Eigen::MatrixXd::Zero(m_prb_dim, m_prb_dim);
